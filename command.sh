78f11d6be58d97c1bad69f8ec436d76b01ec1679
Satyampandey1234 <86377317+Satyampandey1234@users.noreply.github.com>
2026-08-19 14:03:34 +0530

    Add deployment and service for dev-deployment2

--- command.sh
+++ command.sh
@@ -3,3 +3,5 @@ kubectl apply -f dev-secret.yaml
 kubectl apply -f web-config-cm.yaml
 kubectl apply -f dev-deployment.yaml
 kubectl apply -f dev-service.yaml
+kubectl apply -f dev-deployment2.yaml
+kubectl apply -f dev-service2.yaml
