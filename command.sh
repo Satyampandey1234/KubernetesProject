241cbb8372c71a1da1a59f954f6a4f0e34c2b2f7
Satyampandey1234 <86377317+Satyampandey1234@users.noreply.github.com>
2026-08-19 14:05:26 +0530

    Update kubectl command to use secret.yaml

--- command.sh
+++ command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl apply -f dev-secret.yaml
+kubectl apply -f secret.yaml
 kubectl apply -f web-config-cm.yaml
 kubectl apply -f dev-deployment.yaml
 kubectl apply -f dev-service.yaml
